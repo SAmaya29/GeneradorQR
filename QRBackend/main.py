ca5b00feee95247c75c476611ea7bee91cf546ca
Sebastian Amaya Perez <sebastian.amaya1@udea.edu.co>
2025-07-27 00:53:55 -0500

    Listo para despliegue

---
+++ QRBackend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import qrcode
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/generateQR/")
+def generateQR(url: str):
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
